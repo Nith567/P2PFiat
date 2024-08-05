74d8940818812ebea35dc0f2f014365e1a5769de
Nithin <nithinkatkam504106@gmail.com>
2024-08-05 15:32:22 +0530

    logo setup

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 
+
 ## P2PFiat 
+
+![alt text](image.png)
+
 >>`` Powered by Pyth Oracle, EAS, SUPERCHAIN, WorldCoin, CCIP, BlockScout ``
 
--- components/providers.tsx
+++ components/providers.tsx
@@ -12,7 +12,7 @@ export default function Providers({children}: {children: React.ReactNode}) {
         appearance: {
           theme: 'dark',
           accentColor: '#676FF6',
-          logo: '',
+          logo: 'https://shorturl.at/QMf6T',
         },
         embeddedWallets: {
           createOnLogin: 'users-without-wallets',
